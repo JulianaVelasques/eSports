4fc0460b337c5161420777af145037bf678b5b25
JulianaV <julianabalta16@gmail.com>
2022-09-27 23:05:17 -0300

    feat: create all the routes that we will use in the project

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,13 +14,22 @@ app.post('/ads', (request, response) => {
 });
 
 //listar anúncios de um game específico, não todos
-app.get('/ads', (request, response) => {
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: 'Anúncio' },
     { id: 2, name: 'Anúncio' },
     { id: 3, name: 'Anúncio' },
     { id: 4, name: 'Anúncio' },
   ]);
+});
+
+//pegar de um ads específico um discord
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+
+  return response.json([]);
 });
 
 app.listen(3333);
