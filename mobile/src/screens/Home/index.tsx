1da55dd47ddb2a8f9273fd48faebe1195ab7c361
JulianaV <julianabalta16@gmail.com>
2022-11-01 21:22:17 -0300

    feat: create type of navigation and share data between home and game component

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { Image, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
 export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
 
   useEffect(() => {
     fetch('http://172.21.5.129:3333/games')
@@ -20,19 +28,21 @@ export function Home() {
   }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Image source={logoImg} style={styles.logo} />
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logoImg} style={styles.logo} />
 
-      <Heading title="Encontre o seu Duo" subtitle="Selecione o game que deseja jogar..." />
+        <Heading title="Encontre o seu Duo" subtitle="Selecione o game que deseja jogar..." />
 
-      <FlatList
-        data={games}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <GameCard data={item} />}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        contentContainerStyle={styles.contentList}
-      />
-    </SafeAreaView>
+        <FlatList
+          data={games}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <GameCard data={item} onPress={() => handleOpenGame(item)} />}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
+      </SafeAreaView>
+    </Background>
   );
 }
